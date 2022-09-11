5097fb8d6ed0657370364254b0550ad718fa358c
itsoluwatobby <oluwasamuel050@gmail.com>
2022-09-11 06:35:42 +0100

    seeing if changes takes effect

--- Portfolio website/index.js
+++ Portfolio website/index.js
@@ -8,6 +8,15 @@ const prevEl = document.querySelector(".prev")
 const nextEl = document.querySelector(".next")
 const imageEl = document.querySelector(".images-container")
 const imgEl = document.querySelectorAll("img")
+const hourEl = document.getElementById("hour")
+const minutesEl = document.getElementById("minutes")
+const secondsEl = document.getElementById("seconds")
+const hourTextEl = document.querySelector(".hours")
+const minuteTextEl = document.querySelector(".minute")
+const secondTextEl = document.querySelector(".second")
+const clockEl = document.querySelector(".clock")
+const ampmEl = document.getElementById("ampm")
+
 
 let date = new Date();
 footerEl.innerText = date.getFullYear();
@@ -15,10 +24,12 @@ footerEl.innerText = date.getFullYear();
 educationEl.addEventListener('click', () => {
    educationEl.classList.toggle('active')
    contactEl.classList.remove('active')
+   clockEl.classList.toggle('active')
 })
 contactEl.addEventListener('click', () => {
    contactEl.classList.toggle('active')
    educationEl.classList.remove('active')
+   clockEl.classList.toggle('active')
 })
 
 window.addEventListener("scroll", () => {
@@ -31,6 +42,33 @@ window.addEventListener("scroll", () => {
       listContainerEl.classList.remove("change")
    }
 })
+
+function updateClock() {
+   let date = new Date()
+
+   let h = date.getHours()
+   let minutes = date.getMinutes()
+   let seconds = date.getSeconds()
+   let amOrpm = h > 12 ? 'PM' : 'AM'
+
+   h = h > 12 ? (h % 12) : h; 
+
+   hourTextEl.innerText = h == 1 ? hourTextEl.innerText = 'HOUR' : hourTextEl.innerText = 'HOURS';
+   minuteTextEl.innerText = minutes == 1 ? minuteTextEl.innerText = 'MINUTE' : minuteTextEl.innerText = 'MINUTES';
+   secondTextEl.innerText = seconds == 1 ? secondTextEl.innerText = 'SECOND' : secondTextEl.innerText = 'SECONDS';
+
+   hourEl.innerText = format(h);
+   minutesEl.innerText = format(minutes)
+   secondsEl.innerText = format(seconds)
+   ampmEl.innerText = amOrpm
+
+   function format(x) {
+      let y = x < 10 ? '0'+x : x
+      return y
+   }
+}
+
+setInterval(updateClock, 1000)
 
 let currentImg = 1
 prevEl.addEventListener('click', () => {
